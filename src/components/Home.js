3ee47d83260dde78991fdd77475e9bd6b5228c53
Keller Johnson <kjlt4412@gmail.com>
2017-11-04 18:27:55 -0600

    Home page completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -60,6 +60,66 @@ makes it unmistakable in all the right ways.</div>
                     </div>
             </div>
             </section>
+            <section className="fourthSection">
+                <div className="layerSmarter">
+                    <div className="layerContainer">
+                    <div className="layerTitle">LAYER SMARTER</div>
+                    <br></br>
+                    <div className="body2">From base layer to outerwear, take your workouts up a notch with innovative
+                        fabrics made to keep you warm, cool and dry while you move outside.</div>
+                        <br></br>
+                        <br></br>
+                    <div className="shopNow">SHOP NOW</div>
+                </div>
+                </div>
+                    <div className="layerSmarter2"></div>
+            </section>
+            <section className="thirdSection">
+                <div className="thirdSectionTitle">
+                    SUEDE FOR WINTER
+                </div>
+            <div className="placeholderContainer">
+                <div className="placeholder">
+                    <div className="pic5"></div>
+                    <div className="textContain">
+                    <p>Nike Classic Cortez Suede</p>
+                    <p>Women's Shoe</p>
+                    <p>Shop Now</p>
+                    </div>
+                    </div>
+                <div className="placeholder">
+                    <div className="pic6"></div>
+                    <div className="textContain">                
+                    <p>Nike Air Huarache SD</p>
+                    <p>Women's Shoe</p>
+                    <p>Shop Now</p>
+                    </div>
+                    </div>
+                <div className="placeholder">
+                    <div className="pic7"></div>  
+                    <div className="textContain">                                  
+                    <p>Nike Air Force 1 07 LV8 Suede</p>
+                    <p>Men's Shoe</p>
+                    <p>Shop Now</p>
+                    </div>
+                    </div>
+                <div className="placeholder">
+                    <div className="pic8"></div> 
+                    <div className="textContain">                                       
+                    <p>Nike Air Force 1 High '07 LV8 Suede</p>
+                    <p>Men's Shoe</p>
+                    <p>Shop Now</p>
+                    </div>
+                    </div>
+            </div>
+            </section>
+            <section className="lastSection">
+                <div className="lastSectionContainer">
+                    <div className="jackets"></div>
+                    <div className="fleece"></div>
+                    <div className="tights"></div>
+                </div>
+            </section>
             </main>
         )
     }
